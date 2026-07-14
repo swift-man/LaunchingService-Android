9e0f676d23e7ed2e9878527c6d673792015a40e1
swift-man <jiniopening@naver.com>
2026-07-14 16:37:47 +0900

    docs. Dokka API 문서 배포 구성
    Dokka 2.2 HTML 생성을 Android 라이브러리 모듈에 연결합니다.
    
    생성 결과를 swift-man/docs main 브랜치의 LaunchingService-Android 전용 경로로 배포하고, 운영 및 리뷰 규칙을 문서화합니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.maven.publish) apply false
+  alias(libs.plugins.dokka) apply false
 }
